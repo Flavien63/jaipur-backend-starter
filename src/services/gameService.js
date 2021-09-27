44e8893e4225ab02bf753d9f594a74518fba9de8
fldermigny <fldermigny@a112pc25L.local.isima.fr>
2021-09-27 20:32:19 +0200

    Ajout du chemin vers la création de jeu

---
+++ src/services/gameService.js
@@ -0,0 +1,24 @@
+import * as databaseService from "./databaseService"
+import { shuffle } from "lodash"
+
+// Return a shuffled starting deck except 3 camels
+function initDeck() {
+  
+  return []
+}
+
+// Draw {count} cards of a deck
+function drawCards(deck, count = 1) {
+  // TODO
+}
+
+// Transfer camels from players hand (_players[i].hand) to their herd (_players[i].camelsCount)
+function putCamelsFromHandToHerd(game) {
+  // TODO
+}
+
+// Create a game object
+export function createGame(name) {
+  // TODO
+  return {}
+}
