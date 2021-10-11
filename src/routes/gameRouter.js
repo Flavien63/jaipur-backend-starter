459adfbe0cd5005c36e65a682fd47e94e69ee222
fldermigny <fldermigny@a112pc24L.local.isima.fr>
2021-10-11 21:01:22 +0200

    Correction des erreurs de tests, problème de majuscule

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -3,16 +3,12 @@ import * as gameService from "../services/gameService"
 
 const router = express.Router()
 
-// Listen to POST /games
 router.post("/", function (req, res) {
   if (!req.body.name) {
     return res.status(400).send("Missing name parameter")
   }
-  // return 400 if req.body.name doesn't exist
-  else {
-    const newGame = gameService.createGame(req.body.name)
-    res.status(201).json(newGame)
-  }
+  const newGame = gameService.createGame(req.body.name)
+  res.status(201).json(newGame)
 })
 
 export default router
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -3,14 +3,14 @@ import { shuffle } from "lodash"
 
 // Return a shuffled starting deck except 3 camels
 export function initDeck() {
-  const deck = Array(6)
-    .fill("Diamonds")
-    .concat(Array(6).fill("Gold"))
-    .concat(Array(6).fill("Silver"))
-    .concat(Array(8).fill("Cloth"))
-    .concat(Array(8).fill("Spice"))
-    .concat(Array(10).fill("Leather"))
-    .concat(Array(11 - 3).fill("Camel"))
+  const deck = []
+  for (let i = 0; i < 6; i++) deck.push("diamonds")
+  for (let i = 0; i < 6; i++) deck.push("gold")
+  for (let i = 0; i < 6; i++) deck.push("silver")
+  for (let i = 0; i < 8; i++) deck.push("cloth")
+  for (let i = 0; i < 8; i++) deck.push("spice")
+  for (let i = 0; i < 10; i++) deck.push("leather")
+  for (let i = 0; i < 11 - 3; i++) deck.push("camel")
   return shuffle(deck)
 }
 
@@ -38,16 +38,15 @@ export function putCamelsFromHandToHerd(game) {
 // Create a game object
 export function createGame(name) {
   const deck = initDeck()
-  const market = ["Camel", "Camel", "Camel", ...drawCards(deck, 2)]
+  const market = ["camel", "camel", "camel", ...drawCards(deck, 2)]
   const game = {
-    // identifiant de la partie
     id: databaseService.getGames().length + 1,
     name,
     market,
     _deck: deck,
     _players: [
       { hand: drawCards(deck, 5), camelsCount: 0, score: 0 },
-      { hand: [], camelsCount: 0, score: 0 },
+      { hand: drawCards(deck, 5), camelsCount: 0, score: 0 },
     ],
     currentPlayerIndex: 0,
     tokens: {
@@ -58,23 +57,14 @@ export function createGame(name) {
       spice: [5, 3, 3, 2, 2, 1, 1],
       leather: [4, 3, 2, 1, 1, 1, 1, 1, 1],
     },
-    // ne pas oublier de les mélanger au début de la partie
     _bonusTokens: {
-      3: [2, 1, 2, 3, 1, 2, 3],
-      4: [4, 6, 6, 4, 5, 5],
-      5: [8, 10, 9, 8, 10],
+      3: shuffle([2, 1, 2, 3, 1, 2, 3]),
+      4: shuffle([4, 6, 6, 4, 5, 5]),
+      5: shuffle([8, 10, 9, 8, 10]),
     },
-    // est-ce que la partie est terminée?
     isDone: false,
   }
-
-  game._players[0].concat(drawCards(game._deck, 5))
   putCamelsFromHandToHerd(game)
-  game.currentPlayerIndex = 1
-  game._players[1].concat(drawCards(game._deck, 5))
-  putCamelsFromHandToHerd(game)
-  game.currentPlayerIndex = 0
   databaseService.saveGame(game)
-
   return game
 }
